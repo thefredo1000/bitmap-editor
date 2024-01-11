c6800f9e5e46c278f203e55f46744da3ead77dd3
Rodrigo Casale <popobasura3@hotmail.com>
2024-01-11 00:12:46 -0600

    app and index setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Center, Heading } from "@chakra-ui/react";
+import Editor from "./components/Editor";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Center bgGradient="linear(#232526 0%, #414345 75%)" h="100vh" w="100vw">
+      <Box bg="white" p={8} borderRadius="lg" w="80vw" minH="75vh">
+        <Heading>Bitmap Editor</Heading>
+        <Editor />
+      </Box>
+    </Center>
   );
 }
 
