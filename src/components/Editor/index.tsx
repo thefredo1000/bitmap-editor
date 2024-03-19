5db4a99f89ec66bf21832488d590af0fdc7f34f8
Rodrigo Casale <popobasura3@hotmail.com>
2024-03-19 00:38:09 -0600

    editor changes

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,70 +1,146 @@
-import { Box, HStack, Input, Text } from "@chakra-ui/react";
+import { Button, HStack, useDisclosure } from "@chakra-ui/react";
 import PixelGrid from "../PixelGrid";
-import { useState } from "react";
-import ColorPicker from "../ColorPicker";
+import { useEffect, useState } from "react";
 import { Color } from "../../types/colors";
-import { convertColorToCSSColor } from "../../utils/conversions";
 import { Size } from "../../types/grid";
+import SizeEditorModal from "../SizeEditorModal";
+import ColorPickerModal from "../ColorPickerModal.tsx";
+import Palette from "../Palette";
+import {
+  convertColorToCSSColor,
+  createBmp,
+  cssRgbToArray,
+} from "../../utils/conversions";
 
 export default function Editor() {
-  const [selectedColor, setSelectedColor] = useState<Color>({
-    RED: 0,
-    GREEN: 0,
-    BLUE: 0,
-  });
+  const [selectedColorIndex, setSelectedColorIndex] = useState<number>(0);
 
   const [size, setSize] = useState<Size>({
     width: 16,
     height: 16,
   });
 
+  const [grid, setGrid] = useState(
+    new Array(size.width ?? 4)
+      .fill(null)
+      .map(() => new Array(size.height ?? 4).fill("rgb(0,0,0)"))
+  );
+  const [colors, setColors] = useState<Color[]>([
+    { RED: 0, GREEN: 0, BLUE: 0 },
+    { RED: 51, GREEN: 51, BLUE: 51 },
+    { RED: 51, GREEN: 0, BLUE: 0 },
+    { RED: 0, GREEN: 51, BLUE: 0 },
+    { RED: 0, GREEN: 0, BLUE: 51 },
+    { RED: 51, GREEN: 51, BLUE: 0 },
+    { RED: 51, GREEN: 0, BLUE: 51 },
+    { RED: 0, GREEN: 51, BLUE: 51 },
+  ]);
+
+  const {
+    isOpen: isSizeEditorOpen,
+    onOpen: onSizeEditorOpen,
+    onClose: onSizeEditorClose,
+  } = useDisclosure();
+  const {
+    isOpen: isColorPickerOpen,
+    onOpen: onColorPickerOpen,
+    onClose: onColorPickerClose,
+  } = useDisclosure();
+
+  const handleColorChange = (color: Color) => {
+    const newColors = [...colors];
+    newColors[selectedColorIndex] = color;
+    setColors(newColors);
+  };
+
+  const openColorPicker = (index: number) => {
+    setSelectedColorIndex(index);
+    onColorPickerOpen();
+  };
+
+  useEffect(() => {
+    setGrid(
+      new Array(size.width)
+        .fill(null)
+        .map(() =>
+          new Array(size.height).fill(
+            convertColorToCSSColor(colors[selectedColorIndex])
+          )
+        )
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [size]);
+
   return (
-    <HStack>
-      <Box w="260px">
-        <Text> Size: </Text>
-        <Input
-          variant="flushed"
-          placeholder="width (px)"
-          onChange={(event) => {
-            setSize({
-              ...size,
-              width: isNaN(parseInt(event.target.value))
-                ? 4
-                : parseInt(event.target.value),
+    <>
+      <HStack my={2}>
+        <Button
+          colorScheme={"green"}
+          onClick={() => {
+            const numericGrid = grid.map((row) => row.map(cssRgbToArray));
+            const buffer = createBmp(grid.length, grid[0].length, numericGrid);
+            // Convert the buffer to a Blob
+            const blob = new Blob([buffer], {
+              type: "application/octet-stream",
             });
+
+            // Create an object URL for the blob
+            const url = window.URL.createObjectURL(blob);
+
+            // Create an anchor (<a>) element
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = "pixel-art.bmp";
+
+            // Append the anchor to the document body
+            document.body.appendChild(a);
+
+            // Programmatically click the anchor to trigger the download
+            a.click();
+
+            // Remove the anchor from the document
+            document.body.removeChild(a);
+
+            // Release the object URL
+            window.URL.revokeObjectURL(url);
           }}
-          defaultValue={size.width.toString()}
-        />
-        <Input
-          variant="flushed"
-          placeholder="heigth (px)"
-          onChange={(event) => {
-            setSize({
-              ...size,
-              height: isNaN(parseInt(event.target.value))
-                ? 4
-                : parseInt(event.target.value),
-            });
+        >
+          Download
+        </Button>
+        <Button
+          colorScheme={"yellow"}
+          onClick={() => {
+            onSizeEditorOpen();
           }}
-          defaultValue={size.height.toString()}
+        >
+          Change Size
+        </Button>
+        <Palette
+          selectedColorIndex={selectedColorIndex}
+          setSelectedColorIndex={setSelectedColorIndex}
+          colors={colors}
+          openColorPicker={openColorPicker}
         />
-      </Box>
-      <Box w="260px">
-        <Text> Selected Color: </Text>
-        <Text>
-          {selectedColor.RED}, {selectedColor.GREEN}, {selectedColor.BLUE}
-        </Text>
-        <Box
-          h="10px"
-          w="10px"
-          bgColor={convertColorToCSSColor(selectedColor)}
-        />
-        <ColorPicker
-          selectedColor={selectedColor}
-          setSelectedColor={setSelectedColor}
-        />
-      </Box>
-      <PixelGrid selectedColor={selectedColor} size={size} />
-    </HStack>
+      </HStack>
+
+      <PixelGrid
+        grid={grid}
+        setGrid={setGrid}
+        selectedColor={colors[selectedColorIndex]}
+        size={size}
+      />
+      <ColorPickerModal
+        isOpen={isColorPickerOpen}
+        onClose={onColorPickerClose}
+        selectedColor={colors[selectedColorIndex]}
+        setSelectedColor={handleColorChange}
+      />
+      <SizeEditorModal
+        isOpen={isSizeEditorOpen}
+        onClose={onSizeEditorClose}
+        setSize={setSize}
+        size={size}
+      />
+    </>
   );
 }
